1bd6a80c59d30ee3ed6ae5a2e7ab32bd1796dee7
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2019-07-17 03:50:11 +0200

    Add cloudfront distribution
    This enables cache and HTTPS without a custom domain

---
+++ cloudfront.tf
@@ -0,0 +1,130 @@
+resource "aws_cloudfront_distribution" "wordpress" {
+  enabled     = true
+  price_class = "PriceClass_200"
+
+  origin {
+    domain_name = "${aws_lb.wordpress.dns_name}"
+    origin_id   = "alb"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT",
+    ]
+
+    cached_methods = ["GET", "HEAD"]
+
+    target_origin_id = "alb"
+    compress         = true
+
+    forwarded_values {
+      headers = ["*"]
+
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 0
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "wp-includes/*"
+
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT",
+    ]
+
+    cached_methods = ["GET", "HEAD"]
+
+    target_origin_id = "alb"
+    compress         = true
+
+    forwarded_values {
+      headers      = ["Host"]
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+
+    default_ttl = 900
+    min_ttl     = 900
+    max_ttl     = 900
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "wp-content/*"
+
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT",
+    ]
+
+    cached_methods = ["GET", "HEAD"]
+
+    target_origin_id = "alb"
+    compress         = true
+
+    forwarded_values {
+      headers      = ["Host"]
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+
+    default_ttl = 900
+    min_ttl     = 900
+    max_ttl     = 900
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = "${var.tags}"
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "lb_dns_name" {
   value = "${aws_lb.wordpress.dns_name}"
 }
+
+output "cf_dns_name" {
+  value = "${aws_cloudfront_distribution.wordpress.domain_name}"
+}
