f900f5980fa430ba3aa8b6eda11ca986c43916d1
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2019-07-17 03:44:52 +0200

    Set DHCP options to associate R53 Zone

--- base/vpc.tf
+++ base/vpc.tf
@@ -10,3 +10,15 @@ resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
   tags   = "${var.tags}"
 }
+
+resource "aws_vpc_dhcp_options" "default" {
+  domain_name         = "${var.internal_domain_name}"
+  domain_name_servers = ["AmazonProvidedDNS"]
+
+  tags = "${var.tags}"
+}
+
+resource "aws_vpc_dhcp_options_association" "default" {
+  vpc_id          = "${aws_vpc.default.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.default.id}"
+}
