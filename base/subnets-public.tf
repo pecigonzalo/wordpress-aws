04b9052be57bd6b23c6b59aa91ad1d154573c007
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2019-07-17 03:40:58 +0200

    Fix public routing association
    Signed-off-by: Gonzalo Peci <pecigonzalo@users.noreply.github.com>

--- base/subnets-public.tf
+++ base/subnets-public.tf
@@ -35,6 +35,6 @@ resource "aws_route" "public" {
 resource "aws_route_table_association" "public" {
   count = "${length(var.availability_zones)}"
 
-  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.public.*.id, count.index)}"
 }
